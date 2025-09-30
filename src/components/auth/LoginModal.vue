e4052dc678b1d1551c5c172561e5297847e53a3b
Gabriel Matheus Silva <gabrielmath@hotmail.com>
2025-09-30 19:45:18 -0300

    wip

--- src/components/auth/LoginModal.vue
+++ src/components/auth/LoginModal.vue
@@ -7,6 +7,7 @@ import AuthForm from '@/components/auth/AuthForm.vue'
 import RegisterForm from '@/components/auth/RegisterForm.vue'
 
 const open = ref(false)
+const formCreate = ref<boolean>(false)
 const email = ref('')
 const password = ref('')
 const loading = ref(false)
@@ -31,27 +32,27 @@ async function handleLogin() {
 <template>
   <Button variant="secondary" @click="open = true"> Entrar/Criar conta</Button>
 
-  <Modal :show="open" title="Entre na sua conta" @close="open = false">
-    <!--    <AuthForm />-->
-    <RegisterForm />
-    <!--    <div class="flex flex-col gap-4">
-          <Input v-model="email" label="E-mail" placeholder="exemplo@gmail.com" />
-          <Input v-model="password" label="Senha" placeholder="Digite sua senha" type="password" />
-          <p v-if="errorMessage" class="text-danger-2 text-sm">{{ errorMessage }}</p>
-        </div>
+  <Modal
+    :show="open"
+    :title="formCreate ? 'Crie sua conta' : 'Entre na sua conta'"
+    @close="open = false"
+  >
+    <RegisterForm v-if="formCreate" />
+    <AuthForm v-else />
 
-        <div class="flex flex-col w-full pt-8 pb-4">
-          <Button class="w-full mb-4" :disabled="loading" @click="handleLogin" size="big">
-            {{ loading ? 'Entrando...' : 'Entrar' }}
-          </Button>
-        </div>-->
-
-    <!-- Link criar conta -->
     <p
+      v-if="formCreate"
+      class="flex justify-center items-center space-x-2 text-grayScale-3 pt-6 text-center border-t border-t-grayScale-1"
+    >
+      <span>Já tem uma conta?</span>
+      <Button variant="secondary" @click="formCreate = false"> Entrar na conta</Button>
+    </p>
+    <p
+      v-else
       class="flex justify-center items-center space-x-2 text-grayScale-3 pt-6 text-center border-t border-t-grayScale-1"
     >
       <span>Não tem uma conta?</span>
-      <Button variant="secondary"> Criar uma conta</Button>
+      <Button variant="secondary" @click="formCreate = true"> Criar uma conta</Button>
     </p>
   </Modal>
 </template>
